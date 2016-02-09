a58ce8cd44bd5a02544880c1494f64f63767cf9b
beatle <kudgo.test@gmail.com>
2016-02-10 01:13:31 +0200

    Initial implementation

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 /* jshint node: true */
 'use strict';
+var Promise = require('ember-cli/lib/ext/promise');
+var portfinder = require("portfinder");
+var getPort = Promise.denodeify(portfinder.getPort);
 
 module.exports = {
-  name: 'ember-cli-auto-port'
+  name: 'ember-cli-auto-port',
+
+  serverMiddleware: function(startOptions) {
+    portfinder.basePort = startOptions.options.port;
+    return getPort({
+      host: startOptions.options.host,
+      basePort: startOptions.options.port
+    }).then(function(port) {
+      startOptions.options.port = port;
+    }.bind(this));
+  }
 };
